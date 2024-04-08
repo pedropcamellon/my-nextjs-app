ee86feccfe57de1e05912d60f2bc5823a4ffc34b
Pedro P. Camellon <pedropcamellon@gmail.com>
2024-04-07 22:18:54 -0400

    Search rooms

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,21 +1,10 @@
-// import { getFeaturedRoom } from "@/libs/apis";
-// import FeaturedRoom from "@/components/FeaturedRoom/FeaturedRoom";
 import Gallery from "@/components/Gallery/Gallery";
 import HeroSection from "@/components/HeroSection/HeroSection";
-// import PageSearch from "@/components/PageSearch/PageSearch";
 
 const Home = async () => {
-  // const featuredRoom = await getFeaturedRoom();
-
   return (
     <>
       <HeroSection />
-
-      {/* TODO: Add PageSearch */}
-      {/* <PageSearch /> */}
-
-      {/* TODO: Add FeaturedRoom */}
-      {/* <FeaturedRoom featuredRoom={featuredRoom} /> */}
 
       <Gallery />
     </>
